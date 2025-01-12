a2dd254bfbe55bc3280db62f10abff66cd4ede58
Luan de Oliveira <luan012012@hotmail.com>
2025-01-12 00:01:23 -0300

    fix test

--- greetings/greetings_test.go
+++ greetings/greetings_test.go
@@ -1,22 +1,22 @@
 package greetings
 
-import(
-	"testing"
+import (
 	"regexp"
+	"testing"
 )
 
-func TestHelloName(t *testing.T){
-	name:= "Luan"
-	want:= regexp.MustCompile(`\b`+name+`\b`)
+func TestHelloName(t *testing.T) {
+	name := "Luan"
+	want := regexp.MustCompile(`\b` + name + `\b`)
 	msg, err := Hello("Luan")
 	if !want.MatchString(msg) || err != nil {
-		t.Fatalf(`Hello("Gladys") =  %q, %v, want match for %#q, nil, msg, err, want`)
+		t.Fatalf(`Hello("Luan") =  %q, %v, want match for %#q, nil, msg, err, want`)
 	}
 }
 
-func TestHelloEmpty(t *testing.T){
-		msg, err := Hello("")
-		if msg != ""|| err != nil{
-			t.Fatalf(`Hello("")` = %q, %v, want "", error `,msg ,err)
-		}
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err != nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error `, msg, err)
+	}
 }
