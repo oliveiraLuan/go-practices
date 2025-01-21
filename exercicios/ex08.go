e4ae4beafaab6ff0d8ed1507afa997f2323803df
Luan de Oliveira <luan012012@hotmail.com>
2025-01-21 08:46:59 -0300

    refactor: lendo resposta do usuário

--- exercicios/ex08.go
+++ exercicios/ex08.go
@@ -3,24 +3,37 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	var stringNumber string
+	var stringNumber, response string
 	var erro error
 	var intNumber int
-	fmt.Println("Digite um número positivo e maior que 1: ")
-	fmt.Scan(&stringNumber)
 
-	intNumber, erro = strconv.Atoi(stringNumber)
-
-	for erro != nil || intNumber <= 0 {
-		fmt.Println("Erro, digite um número inteiro e positivo.")
+	for response != "N" {
+		fmt.Println("Digite um número positivo e maior que 1: ")
 		fmt.Scan(&stringNumber)
-		intNumber, erro = strconv.Atoi(stringNumber)
-	}
 
-	fmt.Print("O resultado é: ", fat(intNumber))
+		intNumber, erro = strconv.Atoi(stringNumber)
+
+		for erro != nil || intNumber <= 0 {
+			fmt.Println("Erro, digite um número inteiro e positivo.")
+			fmt.Scan(&stringNumber)
+			intNumber, erro = strconv.Atoi(stringNumber)
+		}
+
+		fmt.Println("O resultado é: ", fat(intNumber))
+
+		fmt.Println("Desejar continuar? [S/N]:")
+		fmt.Scan(&response)
+
+		for response != "S" && response != "N" {
+			fmt.Println("Caractere inválido, por favor escolha entre sim ou não com as respectivas letras [S] ou [N]:")
+			fmt.Scan(&response)
+			response = strings.ToUpper(response)
+		}
+	}
 }
 
 func fat(number int) int {
